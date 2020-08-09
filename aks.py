6eb6ab3dcac51957a00e33d5f363b6e5d4f15749
Tone Def <tonedefdev@gmail.com>
2020-08-08 21:56:09 -0700

    Moving cluster_names to the init function

--- aks.py
+++ aks.py
@@ -5,12 +5,12 @@ from genetic import Genetics
 class AKS(Genetics):
     def __init__(self, cluster_count, resource_group_name, location, project, environment, tags):
         self.cluster_count = cluster_count
+        self.cluster_names = []
         super().__init__(resource_group_name, location, project, environment, tags)
 
     def create_cluster(self):
         aks_name = f'{self.project}-aks-{self.environment}'
         dns_prefix = f'{aks_name}-{self.environment}'
-        self.cluster_names = []
 
         for i in range(self.cluster_count):
             cluster_name = f'{aks_name}{i}'
